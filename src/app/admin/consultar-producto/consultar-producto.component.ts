1ef6b5d06719a15ec9b78054a5e1fe3c3ff61327
matias25pinto <matias25pinto@gmail.com>
2022-02-15 22:37:59 -0300

    REFACTOR(consultar-producto):Se agrego el evento de click al icono de busqueda

--- src/app/admin/consultar-producto/consultar-producto.component.ts
+++ src/app/admin/consultar-producto/consultar-producto.component.ts
@@ -7,10 +7,7 @@ import { ProductosService } from 'src/app/core/shared/services/productos.service
   selector: 'app-consultar-producto',
   templateUrl: './consultar-producto.component.html',
 })
-
-
 export class ConsultarProductoComponent implements OnInit {
- 
   public termino: string = '';
   public desde: string;
   public pagina: number;
@@ -30,53 +27,54 @@ export class ConsultarProductoComponent implements OnInit {
   };
 
   @ViewChild('someInput') someInput: ElementRef;
- 
 
-  constructor( private productosService: ProductosService, private fb:FormBuilder) { 
-    this.formulario = this.fb.group({termino:['', Validators.required]});
+  constructor(
+    private productosService: ProductosService,
+    private fb: FormBuilder
+  ) {
+    this.formulario = this.fb.group({ termino: ['', Validators.required] });
   }
 
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
 
   ngAfterViewInit() {
     this.someInput.nativeElement.focus();
   }
 
-  enviarFormulario(){
-    if(this.formulario.valid){
+  enviarFormulario() {
+    if (this.formulario.valid) {
       this.isLoading = true;
-      this.termino = this.formulario.get('termino')?.value || 'el término de búsqueda';
+      this.termino =
+        this.formulario.get('termino')?.value || 'el término de búsqueda';
       this.desde = '0';
       this.cargarProductos(this.termino, this.desde);
     }
   }
   cargarProductos(termino: string, desde: string) {
+    this.productosService.buscarProducto(termino, desde).subscribe(
+      (resp) => {
+        this.isLoading = false;
 
-    this.productosService.buscarProducto(termino, desde).subscribe((resp) => {
+        this.productos = resp;
 
-      this.isLoading = false;
-      
-      this.productos = resp;
+        if (resp.length === 0) {
+          this.sinProductos = true;
+        } else {
+          this.sinProductos = false;
 
-      if (resp.length === 0) {
-        this.sinProductos = true;
-      } else {
-        this.sinProductos = false;
-
-        if(this.productos.length == 1){
-          this.cargarDetalleProducto(this.productos[0]);
-        }else{
-          this.mostrarProducto = false;
+          if (this.productos.length == 1) {
+            this.cargarDetalleProducto(this.productos[0]);
+          } else {
+            this.mostrarProducto = false;
+          }
         }
+        this.limpiarFormulario();
+      },
+      (err) => {
+        this.productos = [];
+        this.isLoading = false;
       }
-      this.limpiarFormulario();
-    }, 
-    err=>{
-      this.productos = [];
-      this.isLoading = false;
-    });
+    );
   }
 
   cargarDetalleProducto(producto: Producto) {
@@ -84,10 +82,9 @@ export class ConsultarProductoComponent implements OnInit {
     this.detalle = producto;
   }
 
-  limpiarFormulario(){
-    this.formulario.reset({termino:''});
+  limpiarFormulario() {
+    this.formulario.reset({ termino: '' });
   }
-
 
   siguiente() {
     this.desde = (parseInt(this.desde) + 10).toString();
@@ -98,9 +95,8 @@ export class ConsultarProductoComponent implements OnInit {
     this.desde = (parseInt(this.desde) - 10).toString();
     if (parseInt(this.desde) >= 0) {
       this.cargarProductos(this.termino, this.desde);
-    }else{
-      this.desde = "0";
+    } else {
+      this.desde = '0';
     }
   }
-
 }
